23802eee090f17dc9f6a507502fb316f3f85fe12
s예찬 <davidsyc1170@gmail.com>
2023-10-31 20:50:20 +0900

    feat: jpa auditing 기능 추가

--- src/main/java/org/simple/entity/Posts.java
+++ src/main/java/org/simple/entity/Posts.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTime{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
